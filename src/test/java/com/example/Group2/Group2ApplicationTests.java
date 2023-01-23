e447d9ea72d49d8a5f06f74d539dbc576d0e6016
Ecksmas <ludde.oelker@gmail.com>
2023-01-23 14:04:07 +0100

    Inint

---
+++ src/test/java/com/example/Group2/Group2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
