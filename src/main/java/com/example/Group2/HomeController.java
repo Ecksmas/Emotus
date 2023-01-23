9cc50f377372272a2d2a73ecf252b6110db49a3a
Ecksmas <ludde.oelker@gmail.com>
2023-01-23 15:55:35 +0100

    Created SongRepository

--- src/main/java/com/example/Group2/HomeController.java
+++ src/main/java/com/example/Group2/HomeController.java
@@ -1,14 +1,25 @@
 package com.example.Group2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private SongRepository repository;
+
 
     @GetMapping("/")
     public String Start() {
+
+        return "HomePage";
+    }
+
+    @PostMapping("/")
+    public String Search() {
 
         return "HomePage";
     }
---
+++ src/main/java/com/example/Group2/SongRepository.java
@@ -0,0 +1,8 @@
+package com.example.Group2;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SongRepository extends CrudRepository<Song, Long> {
+
+
+}
