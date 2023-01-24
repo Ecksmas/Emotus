77f5463560db1a803ae8e658e46c5bd381ead41a
Ecksmas <ludde.oelker@gmail.com>
2023-01-24 13:56:00 +0100

    Misc commit

--- src/main/java/com/example/Group2/HomeController.java
+++ src/main/java/com/example/Group2/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.Group2;
 
+import kong.unirest.Unirest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -8,10 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class HomeController {
     @Autowired
     private SongService service;
