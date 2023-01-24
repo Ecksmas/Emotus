4ebd6451a8f78caaab8bf926547a64d9a07e24be
Ecksmas <ludde.oelker@gmail.com>
2023-01-24 17:05:06 +0100

    Search now works!!!!

--- src/main/java/com/example/Group2/Group2Application.java
+++ src/main/java/com/example/Group2/Group2Application.java
@@ -1,21 +1,16 @@
 package com.example.Group2;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Group2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Group2Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Group2Application.class, args);
 
-
-/*		HttpResponse<String> response = Unirest.get("https://spotify23.p.rapidapi.com/search/?q=exuma&type=multi&offset=0&limit=10&numberOfTopResults=5")
-				.header("X-RapidAPI-Key", "1c1587b38emsh0e7714653c0660ep10ab75jsnf6e80542980e")
-				.header("X-RapidAPI-Host", "spotify23.p.rapidapi.com")
-				.asString();
-
-		System.out.println(response.getBody());*/
-	}
+    }
 
 }
--- src/main/java/com/example/Group2/HomeController.java
+++ src/main/java/com/example/Group2/HomeController.java
@@ -1,19 +1,18 @@
 package com.example.Group2;
 
+import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class HomeController {
     @Autowired
     private SongService service;
@@ -25,6 +24,20 @@ public class HomeController {
         model.addAttribute("keyword", keyword);
 
         return "HomePage";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model) {
+        HttpResponse<String> response = Unirest.get("https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=2396871&text_format=plain")
+                .header("X-RapidAPI-Key", "1c1587b38emsh0e7714653c0660ep10ab75jsnf6e80542980e")
+                .header("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com")
+                .asString();
+        model.addAttribute("ID", response.getBody());
+
+        //TODO Parse json string
+        //TODO Song id must be song
+
+        return "ResultPage";
     }
 
 }
