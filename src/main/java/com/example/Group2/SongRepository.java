d0640f0da55bc8437b06b1ae412bd73b36929936
Ecksmas <ludde.oelker@gmail.com>
2023-01-24 09:41:23 +0100

    Added LinksForProject.txt

--- src/main/java/com/example/Group2/SongRepository.java
+++ src/main/java/com/example/Group2/SongRepository.java
@@ -2,7 +2,6 @@ package com.example.Group2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
