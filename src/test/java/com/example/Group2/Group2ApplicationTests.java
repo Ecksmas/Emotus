1a4d1ef0704336a2fcf089d59314a9ab1e319e03
Ecksmas <ludde.oelker@gmail.com>
2023-02-02 15:48:49 +0100

    One more test

--- src/test/java/com/example/Group2/Group2ApplicationTests.java
+++ src/test/java/com/example/Group2/Group2ApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.Group2;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -79,7 +84,29 @@ class Group2ApplicationTests {
 
 	@Test
 	public void	songClassAddTest() {
+		HttpResponse<String> songIdResponse = Unirest.get("https://genius-song-lyrics1.p.rapidapi.com/search/?q=" + "happy" + "&per_page=5&page=1")
+				.header("X-RapidAPI-Key", "7fdc5ae974msh4665893b2c77710p1523e0jsn4e77becb0728")
+				.header("X-RapidAPI-Host", "genius-song-lyrics1.p.rapidapi.com")
+				.asString();
 
+		JSONObject json = new JSONObject(songIdResponse.getBody());
+		JSONArray hits = json.getJSONArray("hits");
+
+		List<Song> songList = new ArrayList<>();
+
+		//Loopar igenom sen lägg till
+		for (int i = 0; i < hits.length(); i++) {
+			JSONObject result = hits.getJSONObject(i).getJSONObject("result");
+			String title = result.getString("title");
+			String artist = result.getString("artist_names");
+			String songId = result.getString("id");
+			Song song = new Song(title, artist, songId);
+			songList.add(song);
+		}
+
+		// Picking them in the wrong order.
+		assertEquals(songList.get(0).getTitle(), "Pharrell Williams");
+		assertEquals(songList.get(0).getArtist(), "Happy");
 	}
 
 }
